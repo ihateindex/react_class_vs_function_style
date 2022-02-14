d25db95c78b275a529cdb1adad801a1c825b8eb7
ihateindex <47144148+ihateindex@users.noreply.github.com>
2022-02-14 15:48:00 +0900

    0214 - 함수방식의 setState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
@@ -12,26 +12,57 @@ function App() {
 }
 
 function FuncComp(props) {
+  // * 함수형 state
+  var numberState = useState(props.initNumber);
+  var number = numberState[0];
+  var setNumber = numberState[1];
+
+  // var dateState = useState((new Date()).toString());
+  // var _date = dateState[0];
+  // var setDate = dateState[1];
+
+  // * 함수형 state 축약형
+  var [_date, setDate] = useState((new Date()).toString());
+  
   return (
     <div className='container'>
       <h2>function style component</h2>
-      <p>Number : {props.initNumber}</p>
+      <p>Number : {number}</p>
+      <p>Date : {_date}</p>
+      <input type="button" value="random" onClick={
+          function(){
+            setNumber(Math.random());
+          }
+        }></input>
+      <input type="button" value="date" onClick={
+          function(){
+            setDate((new Date()).toString());
+          }
+        }></input>
     </div>
   );
 }
 
 class ClassComp extends React.Component{
+  // * 클래스형 state
   state = {
-    number:this.props.initNumber
+    number:this.props.initNumber,
+    date: (new Date()).toString()
   }
   render() {
     return (
       <div className='container'>
         <h2>class style component</h2>
         <p>Number : {this.state.number}</p>
+        <p>Date : {this.state.date}</p>
         <input type="button" value="random" onClick={
           function(){
             this.setState({number:Math.random()})
+          }.bind(this)
+        }></input>
+        <input type="button" value="date" onClick={
+          function(){
+            this.setState({date:(new Date()).toString()})
           }.bind(this)
         }></input>
       </div>
